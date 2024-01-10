ad2546cac81e6715fae39d54be3f145a13d36b18
ChaseMoncrieffeMitchem <Chase.mitchem0855@mail.com>
2024-01-10 03:57:19 -0600

    m

--- app.js
+++ app.js
@@ -17,20 +17,23 @@
 // main();
 
 async function main() {
-    const users = await fetch("https://www.omdbapi.com/?apikey=f5bbb04b&s=fast")
+    const users = await fetch("http://www.omdbapi.com/?apikey=f5bbb04b&s=fast")
     const usersData = await users.json();
-    const userListEl = document.querySelector('.user-list');
-    userListEl.innerHTML = usersData.map(user => 
-        `<div class="user-card">
-        <div class="user-card__container">
-            <h3>User's Name</h4>
-            <p><b>Email:</b> email@email.com</p>
-            <p><b>Phone:</b> 0000000000</p>
-            <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+    // const userListEl = document.querySelector('.movie-list');
+    // userListEl.innerHTML = 
+    usersData.Search.map((movie) => 
+        `<div class="movie">
+        <div class="movie-card">
+            <div class="movie-card__container">
+                <h3 class="movie-poster"></h3>
+                <p class="movie-title">Movie Title</p>
+                <p class="movie-type">Movie Type</p>
+                <p class="movie-year">Movie Year</p>
+            </div>
         </div>
-        </div>`
+    </div>`
     ).join("")
-    console.log(usersData)
+    console.log(usersData.Search)
 }
 
 main();
