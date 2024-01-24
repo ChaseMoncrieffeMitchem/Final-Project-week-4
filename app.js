856deb4b9da2571b55e92ae68a751a03b27ebeff
ChaseMoncrieffeMitchem <Chase.mitchem0855@mail.com>
2024-01-23 23:19:11 -0600

    Attemting to debug and get the search bar working

--- app.js
+++ app.js
@@ -17,19 +17,42 @@
 // main();
 
 const userListEl = document.querySelector('.movie-list');
+const movie = localStorage.getItem("movie");
+
+async function onSearchChange(Title) {
+    const movie = event.target.value
+    const users = await fetch(`http://www.omdbapi.com/?apikey=f5bbb04b&s=${Title}`)
+    const usersData = await users.json();
+    userListEl.innerHTML = usersData.map(movie => `<div class="movie">
+    <div class="movie-card" onclick="showMoviePosts(${movie.Title})">
+        <div class="movie-card__container">
+            <h3 class="movie-poster">
+            <img src="${movie.Poster}" alt="" target="_blank"></h3>
+            <p class="movie-title">${movie.Title}</p>
+            <p class="movie-year">${movie.Year}</p>
+        </div>
+    </div>
+</div>`)
+}
 
 async function main() {
-    const users = await fetch("http://www.omdbapi.com/?apikey=f5bbb04b&s=fast")
+    const movie = localStorage.getItem("movie")
+    const users = await fetch(`http://www.omdbapi.com/?apikey=f5bbb04b&s=fast`)
     const usersData = await users.json();
     userListEl.innerHTML = usersData.Search.map((movie) => movieHTML(movie)).join("")
     console.log(usersData)
 }
 
-main();
+main(movie);
+
+function showMoviePosts(movie) {
+    console.log(movie)
+}
+
 
 function movieHTML(movie) {
     return `<div class="movie">
-    <div class="movie-card">
+    <div class="movie-card" onclick="showMoviePosts(${movie.Title})">
         <div class="movie-card__container">
             <h3 class="movie-poster">
             <img src="${movie.Poster}" alt="" target="_blank"></h3>
@@ -40,6 +63,7 @@ function movieHTML(movie) {
 </div>`
 }
 
-function onSearchChange(event) {
-    console.log(event)
+function showMoviePost(movie) {
+    localStorage.setItem("movie", movie)
 }
+
